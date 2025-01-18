1fb6155af0d0b48e20fd8d3376c8f292218a3402
TechQuery <19969570+TechQuery@users.noreply.github.com>
2025-01-18 01:53:54 +0000

    [fix] replace Repository Name

--- components/MainNavigator.tsx
+++ components/MainNavigator.tsx
@@ -28,7 +28,7 @@ export const MainNavigator: FC = observer(() => (
 
           <Nav.Link
             target="_blank"
-            href="https://github.com/idea2app/Next-Bootstrap-TS"
+            href="https://github.com/idea2app/lark-lark-next-bootstrap-ts"
           >
             {t('source_code')}
           </Nav.Link>
